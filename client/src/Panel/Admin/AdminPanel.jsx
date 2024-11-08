980b59093d336193c89b979ac78379809557727a
Agus-Albarracin <abusscin_@hotmail.com>
2024-11-08 05:39:06 -0300

    m

--- client/src/Panel/Admin/AdminPanel.jsx
+++ client/src/Panel/Admin/AdminPanel.jsx
@@ -40,7 +40,7 @@ const AdminPanel = () => {
       try {
         const token = localStorage.getItem('authToken');
 
-        const response = await axios.get('https://disturbiaarg.com/api/admin', {
+        const response = await axios.get('http://localhost:4000/api/admin', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
