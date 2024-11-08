3269f63ee0b5f0cf07680ad676eef28810b612b3
Agus-Albarracin <abusscin_@hotmail.com>
2024-11-08 04:29:59 -0300

    m

--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 4000;
 export const DB_HOST = process.env.DB_HOST || "86.38.202.204";
 export const DB_USER = process.env.DB_USER || "u211881118_disturbia";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "Disturbia0.*";
